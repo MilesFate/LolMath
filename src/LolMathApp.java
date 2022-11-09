effeaee44c18499e2ecde7d930eab647af589222
shy <breaddy16@gmail.com>
2022-11-08 23:12:27 -0600

    Added gui version, renamed LolMathTester to LolMathConsole

---
+++ src/LolMathApp.java
@@ -0,0 +1,55 @@
+/*
+* This is the easiest way to implement this
+* A rough draft is better than not trying
+* I will 100% change this once I spend the time to learn more about swing
+*/
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class LolMathApp {
+    private static final int FRAME_WIDTH = 500;
+    private static final int FRAME_HEIGHT = 500;
+    private static int value = 0;
+
+    public static void main(String[] args)
+    {
+
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        JButton button = new JButton("+");
+        JButton button2 = new JButton("-");
+        JButton button3 = new JButton("Submit");
+        JLabel label = new JLabel("" + value);
+        JLabel label2 = new JLabel("");
+
+        panel.add(button);
+        panel.add(button2);
+        panel.add(button3);
+        panel.add(label);
+        panel.add(label2);
+        frame.add(panel);
+
+        button.addActionListener(e -> {
+            value++;
+            label.setText(""+ value);
+        });
+
+        button2.addActionListener(e -> {
+            value--;
+            label.setText(""+ value);
+        });
+
+        button3.addActionListener(e ->{
+            League lg = new League(value);
+            label2.setText(lg.LevelMath());
+
+        });
+
+        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
